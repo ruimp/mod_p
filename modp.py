a8331b126391043b4a16fcd33164447a06f5823b
anaclb <anacb@protonmail.com>
2020-07-27 00:05:34 +0100

    added line summing method

--- modp.py
+++ modp.py
@@ -3,20 +3,51 @@
 import numpy as np
 
 class Board:
-    def __init__(self, board_size, p):
-        self.board = np.zeros(board_size, dtype=int)
+    def __init__(self, shape, p):
+        self.shape = shape
+        self.board = np.zeros(shape, dtype=int)
         self.p = p
 
-    def sum_line(self, v):
-        inds = np.where((v[:-1] - v[1:] == 0) & (v[:-1] != 0))
-        v[inds] = v[inds] * 2 % self.p
-        v[inds[0][:] + 1] = 0
-        return np.concatenate((v[v != 0], v[v == 0]))
 
+    def sum_left(self, v):
+        nums = v[v != 0]
+        inds = np.where(nums[:-1] - nums[1:] == 0)
+        nums[inds] = nums[inds] * 2 % self.p
+        nums = np.delete(nums, inds[0][:] + 1)
+        return np.concatenate((nums, np.zeros(v.size - nums.size)))
+
+    def sum_right(self, v):
+        nums = v[v != 0]
+        inds = np.where(nums[:-1] - nums[1:] == 0)
+        nums[inds] = nums[inds] * 2 % self.p
+        nums = np.delete(nums, inds[0][:] + 1)
+        return np.concatenate((np.zeros(v.size - nums.size), nums))
+
+    def move_left(self):
+        for i in range(self.shape[0]):
+            self.board[i, :] = self.sum_left(self.board[i, :])
+
+    def move_up(self):
+         for i in range(self.shape[1]):
+            self.board[:, i] = self.sum_left(self.board[:, i])
+
+    def move_right(self):
+        for i in range(self.shape[0]):
+            self.board[i, :] = self.sum_right(self.board[i, :])
+
+    def move_down(self):
+        for i in range(self.shape[1]):
+            self.board[:, i] = self.sum_right(self.board[:, i])
 
 board = Board((4, 4), 5)
 board.board[2, 2] = 2
+board.board[1, 1] = 3
+board.board[0, 1] = 3
+board.board[0, 2] = 2
 board.board[2, 0] = 2
+board.board[1, 2] = 1
 board.board[2, 3] = 4
+
 print(board.board)
-print(board.sum_left())
+board.move_down()
+print(board.board)
